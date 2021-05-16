d603d3d67770558b362e49941409b0cb5224a1f0
引路者 <444141300@qq.com>
2021-05-16 20:13:38 +0800

    Spring WebFlux 使用 R2DBC 访问 MySQL 数据库,当主键且仅当为自增长时id为空时是添加,不为空时是编辑

--- spring_boot_r2dbc.sql
+++ spring_boot_r2dbc.sql
@@ -3,15 +3,15 @@
 
  Source Server         : 192.168.3.66_3306
  Source Server Type    : MySQL
- Source Server Version : 80021
+ Source Server Version : 80023
  Source Host           : 192.168.3.66:3306
  Source Schema         : spring_boot_r2dbc
 
  Target Server Type    : MySQL
- Target Server Version : 80021
+ Target Server Version : 80023
  File Encoding         : 65001
 
- Date: 29/09/2020 22:26:39
+ Date: 16/05/2021 20:04:51
 */
 
 SET NAMES utf8mb4;
@@ -22,29 +22,31 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `student`;
 CREATE TABLE `student`  (
-  `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `id` bigint(0) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '主键',
   `code` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '学号',
   `name` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '姓名',
   `gender` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '性别 	M: 男 F: 女',
   `birthday` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '生日',
   `address` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '家庭地址',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of student
 -- ----------------------------
-INSERT INTO `student` VALUES (1, 'S0001', 'Tom', 'M', '2001-03-05', '东京');
-INSERT INTO `student` VALUES (2, 'S0002', 'Ted', 'M', '2001-06-12', 'USA');
-INSERT INTO `student` VALUES (3, 'S0003', 'Mary', 'F', '1998--9-12', 'china');
+INSERT INTO `student` VALUES (1, '1478741', '田卓智', NULL, '2001-03-05', NULL);
+INSERT INTO `student` VALUES (2, '1478741', '田卓智', NULL, '2001-06-12', NULL);
+INSERT INTO `student` VALUES (3, 'S0003', 'Mary', 'F', '1998-09-12', 'china');
+INSERT INTO `student` VALUES (4, '1478741', '田卓智', NULL, NULL, NULL);
+INSERT INTO `student` VALUES (5, '1478741', '田卓智', NULL, NULL, NULL);
 
 -- ----------------------------
 -- Table structure for sys_user
 -- ----------------------------
 DROP TABLE IF EXISTS `sys_user`;
 CREATE TABLE `sys_user`  (
-  `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '主键',
-  `active` smallint UNSIGNED NOT NULL DEFAULT 1 COMMENT '有效标志',
+  `id` bigint(0) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `active` smallint(0) UNSIGNED NOT NULL DEFAULT 1 COMMENT '有效标志',
   `createdAt` timestamp(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0) COMMENT '创建时间',
   `createdBy` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '创建人',
   `updatedAt` timestamp(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0) COMMENT '更新时间',
@@ -52,9 +54,5 @@ CREATE TABLE `sys_user`  (
   `remark` varchar(512) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '备注',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Records of sys_user
--- ----------------------------
 
 SET FOREIGN_KEY_CHECKS = 1;
